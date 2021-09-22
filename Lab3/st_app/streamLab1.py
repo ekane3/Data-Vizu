1ddc21b970f62c596f6ac3c6f280eef1df76035d
Emile EKANE <ekaneekane3@gmail.com>
2021-09-22 10:00:45 +0200

    requirements file updated

--- Lab3/st_app/streamLab1.py
+++ Lab3/st_app/streamLab1.py
@@ -16,31 +16,32 @@ import matplotlib.pyplot as plt
 
 
 #Title bar
-st.sidebar.title("Currently streaming ❤")
-st.sidebar.markdown("Select the Charts/Plots accordingly:")
-project_title = st.sidebar.text_input("You can change the title of this project")
-if project_title:
-    st.title(project_title)
-else:
-    st.title("Uber raw data April 2014")
-st.markdown("The dashboard will visualize Uber data of April 2014")
-st.markdown("Uber, is an American technology company. Its services include ride-hailing, food delivery, package delivery, couriers, freight transportation, and, through a partnership with Lime, electric bicycle and motorized scooter rental.")
-st.write("""
-### Details of the dataset:
-*The dataset contains information about the Datetime, Latitude, Longitude and Base of each uber ride that happened in the month of July 2014 at New York City, USA.*
+def title_bar():
+    st.sidebar.title("Currently streaming ❤")
+    st.sidebar.markdown("Select the Charts/Plots accordingly:")
+    project_title = st.sidebar.text_input("You can change the title of this project")
+    if project_title:
+        st.title(project_title)
+    else:
+        st.title("Uber raw data April 2014")
+    st.markdown("The dashboard will visualize Uber data of April 2014")
+    st.markdown("Uber, is an American technology company. Its services include ride-hailing, food delivery, package delivery, couriers, freight transportation, and, through a partnership with Lime, electric bicycle and motorized scooter rental.")
+    st.write("""
+    ### Details of the dataset:
+    *The dataset contains information about the Datetime, Latitude, Longitude and Base of each uber ride that happened in the month of July 2014 at New York City, USA.*
 
-Date/Time : The date and time of the Uber pickup  
-Lat : The latitude of the Uber pickup  
-Lon : The longitude of the Uber pickup  
-Base : The TLC base company code affiliated with the Uber pickup
+    Date/Time : The date and time of the Uber pickup  
+    Lat : The latitude of the Uber pickup  
+    Lon : The longitude of the Uber pickup  
+    Base : The TLC base company code affiliated with the Uber pickup
 
-The Base codes are for the following Uber bases:  
-B02512 : Unter  
-B02598 : Hinter  
-B02617 : Weiter  
-B02682 : Schmecken  
-B02764 : Danach-NY  
-""")
+    The Base codes are for the following Uber bases:  
+    B02512 : Unter  
+    B02598 : Hinter  
+    B02617 : Weiter  
+    B02682 : Schmecken  
+    B02764 : Danach-NY  
+    """)
 #LOAD DATA
 #Connecting to endpoint to get csv file 
 
@@ -57,10 +58,12 @@ def load_data():
     return data
 
 df = load_data()
-st.markdown(" #### Let's show a sample of our data, to see what type of data we'll be dealing with.")
-if st.checkbox('Show dataframe'):
-    st.table(df.head(10))
 
+def show_head():
+    st.markdown(" #### Let's show a sample of our data, to see what type of data we'll be dealing with.")
+    if st.checkbox('Show dataframe'):
+        st.table(df.head(10))
+show_head()
 #Functions to get day of month, weekday, hour
 
 def get_dom(dt):
@@ -81,28 +84,34 @@ df['hour'] = df['date/time'].map(get_hour)
 #st.line_chart(df)
 
 #VISUAL REPRESENTATION
-st.subheader('Frequency by date of month')
-#st.map(df)
+def histograms():
+    st.subheader('Frequency by date of month')
 
-#st.dataframe(df.head())
-#some space
-st.markdown("***")
-st.subheader('Histograms of Longitude and latitude')
-fig, ax = plt.subplots(1,2)
-ax[0].hist(df['lat'], bins=60,color='orchid')
-ax[1].hist(df['lon'], bins=60,color='yellow',edgecolor = 'red')
-st.pyplot(fig)
+    #st.dataframe(df.head())
+    #some space
+    st.markdown("***")
+    st.subheader('Histograms of Longitude and latitude')
+    fig, ax = plt.subplots(1,2)
+    ax[0].hist(df['lat'], bins=60,color='orchid')
+    ax[1].hist(df['lon'], bins=60,color='yellow',edgecolor = 'red')
+    st.pyplot(fig)
+histograms()
 
 #COUNTS
+def counts_visu():
+    st.write(df['lon'].value_counts())
+    st.write(df['lat'].value_counts())
 
-st.write(df['lon'].value_counts())
-st.write(df['lat'].value_counts())
+    st.header("Number of pickups per base")
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    plt.figure(figsize=(10,10))
+    fig = sns.countplot(y = df['base'])
+    st.pyplot()
 
-st.header("Number of pickups per base")
-st.set_option('deprecation.showPyplotGlobalUse', False)
-plt.figure(figsize=(10,10))
-fig = sns.countplot(y = df['base'])
-st.pyplot()
+counts_visu()
 
-st.subheader("Mapping the dataset ")
-st.map(df.head(30), zoom=40)
+def maps_visu():
+    st.subheader("Mapping the dataset ")
+    st.map(df.head(30), zoom=40)
+
+maps_visu()
